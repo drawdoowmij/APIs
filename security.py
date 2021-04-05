e04c33a99f644ab0165ee5553bb4d303e5f965ae
drawdoowmij <jimwoodward81@gmail.com>
2021-04-05 15:31:12 -0700

    First commit, added REST API to access items, stores and user authentication

---
+++ security.py
@@ -0,0 +1,11 @@
+# from resources.user import User
+from Models.user import UserModel
+    
+def authenticate(username, password):
+    user = UserModel.find_by_username(username)
+    if user and user.password == password:
+        return user
+
+def identity(payload):
+    userid = payload['identity']
+    return UserModel.find_by_id(userid)
